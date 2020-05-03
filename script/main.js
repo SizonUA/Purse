2db4edeb072a553a6ab20cf0845f85b3a6149a4d
Sizon <sizon@vidrada.org>
2020-05-03 16:15:53 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,15 @@
+const totalBalance = document.querySelector('.total__balance'),
+totalMoneyIncome = document.querySelector('.total__money-income'),
+totalMoneyExpenses = document.querySelector('.total__money-expenses'),
+historyList = document.querySelector('.history__list'),
+operationName = document.querySelector('.operation__name'),
+operationAmount = document.querySelector('.operation__amount'),
+form = document.getElementById('form');
+
+console.log('totalBalance: ', totalBalance);
+console.log('totalMoneyExpenses: ', totalMoneyExpenses);
+console.log('totalMoneyIncome: ', totalMoneyIncome);
+console.log('historyList: ', historyList);
+console.log('operationName: ', operationName);
+console.log('operationAmount: ', operationAmount);
+console.log('form: ', form);
